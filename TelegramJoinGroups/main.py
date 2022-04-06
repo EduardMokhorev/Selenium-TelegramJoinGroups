5f1c0828bc09186a04a7b8a0e0cd38e3a415f654
Eduard Mokhorev <EduardScreenager@yandex.ru>
2022-04-06 18:44:32 +0300

    add:TrueFalse

--- TelegramJoinGroups/main.py
+++ TelegramJoinGroups/main.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException
 import time
 FILE_NAME_PROFILE = r'C:\Users\pc\AppData\Local\Google\Chrome\User Data'
 options = webdriver.ChromeOptions()
@@ -11,6 +12,14 @@ driver = webdriver.Chrome(executable_path="C:\\Users\\chromedriver.exe", chrome_
 wait = WebDriverWait(driver, 500)
 url = "https://web.telegram.org/"
 try:
+
+    def check_exists_by_xpath(xpath):
+        try:
+            driver.find_element(By.XPATH,xpath)
+        except NoSuchElementException:
+            return False
+        return True
+
     urlTelegram = "https://web.telegram.org/"
     urlDinamic = "https://t.me/DeepWeb_Chatt"
     driver.get(url=urlTelegram)
@@ -19,8 +28,13 @@ try:
     time.sleep(2)
     driver.find_element(By.XPATH, '//*[@id="telegram-search-input"]').send_keys(Keys.RETURN)
     time.sleep(2)
-    driver.find_element(By.XPATH, '//*[text()="Join Group"]').click()
-    time.sleep(100)
+    if check_exists_by_xpath('//*[text()="Join Group"]'):
+        driver.find_element(By.XPATH, '//*[text()="Join Group"]').click()
+        print('Have Element')
+        time.sleep(100)
+    else:
+        print("SomeTroubleWith",urlDinamic)
+
 
 except Exception as ex:
     print(ex)
